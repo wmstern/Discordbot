312eaf21b950262fc6ed847fb67739892d316d5c
Arian <ariangonzalez101@gmail.com>
2025-08-30 15:41:03 -0300

    feat: separation of global commands from guild commands

--- src/commands/utils/help.ts
+++ src/commands/utils/help.ts
@@ -1,32 +1,60 @@
-import { Command, Cooldown, DeferReply } from '#framework';
+import { Command, Cooldown, Filters } from '#framework';
 import {
   EmbedBuilder,
   SlashCommandBuilder,
   type ChatInputCommandInteraction
 } from 'discord.js';
 
-const embed = new EmbedBuilder();
+const embeds: Record<PropertyKey, EmbedBuilder> = {};
 
 @Command(
   new SlashCommandBuilder()
     .setName('help')
     .setDescription('Muestra la lista de comandos.')
+    .addSubcommand((sub) =>
+      sub
+        .setName('global')
+        .setDescription('Muestra la lista de comandos del bot.')
+    )
+    .addSubcommand((sub) =>
+      sub
+        .setName('server')
+        .setDescription('Muestra la lista de comandos del servidor.')
+    )
 )
 export class HelpCommand {
-  @DeferReply(true)
   @Cooldown(6000)
-  async run(i: ChatInputCommandInteraction) {
-    if (!embed.data.description) {
+  async global(i: ChatInputCommandInteraction) {
+    if (!('global' in embeds)) {
+      embeds.global = new EmbedBuilder();
       const commands = await (
         await i.client.application.fetch()
       ).commands.fetch();
-      embed.setDescription(
+      embeds.global.setDescription(
         '```\n' + commands.map((cmd) => `/${cmd.name}`).join('\n') + '\n```'
       );
     }
 
-    await i.editReply({
-      embeds: [embed]
+    await i.reply({
+      embeds: [embeds.global]
+    });
+  }
+
+  @Cooldown(6000)
+  @Filters((i) => !i.guild)
+  async server(i: ChatInputCommandInteraction) {
+    if (!i.guild) return;
+
+    if (!(i.guild.id in embeds)) {
+      embeds[i.guild.id] = new EmbedBuilder();
+      const commands = await (await i.guild.fetch()).commands.fetch();
+      embeds[i.guild.id].setDescription(
+        '```\n' + commands.map((cmd) => `/${cmd.name}`).join('\n') + '\n```'
+      );
+    }
+
+    await i.reply({
+      embeds: [embeds[i.guild.id]]
     });
   }
 }
